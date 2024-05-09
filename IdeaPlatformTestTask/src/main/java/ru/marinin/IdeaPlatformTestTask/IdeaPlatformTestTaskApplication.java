4fbbaf49d167cbcb94474c05ebc2bb6e3080d2da
Easyfumi <oleg.marinin.49@yandex.ru>
2024-05-09 12:00:57 +0300

    Initial commit

---
+++ IdeaPlatformTestTask/src/main/java/ru/marinin/IdeaPlatformTestTask/IdeaPlatformTestTaskApplication.java
@@ -0,0 +1,13 @@
+package ru.marinin.IdeaPlatformTestTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdeaPlatformTestTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdeaPlatformTestTaskApplication.class, args);
+	}
+
+}
---
+++ IdeaPlatformTestTask/src/test/java/ru/marinin/IdeaPlatformTestTask/IdeaPlatformTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.marinin.IdeaPlatformTestTask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdeaPlatformTestTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
